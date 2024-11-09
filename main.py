36066e864b1812d3e8d3e3a1d10ae1199ef1d3ba
mexmonjonovuz <asadbekmehmonjonov89@gmail.com>
2024-11-09 20:38:38 +0500

    web hook

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 import os
 import sys
@@ -5,11 +6,6 @@ import sys
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.webhook.aiohttp_server import (
-    SimpleRequestHandler,
-    setup_application,
-)
-from aiohttp import web
 from dotenv import load_dotenv
 
 from bot.starter import begin_router
@@ -17,39 +13,17 @@ from bot.starter import begin_router
 load_dotenv(".env")
 
 TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_PATH = "/webhook"
-WEBHOOK_URL = os.getenv("WEBHOOK_URL") + WEBHOOK_PATH
-BASE_WEBHOOK_URL = "https://a.temur.life"
-WEB_SERVER_PORT = 8000
-WEB_SERVER_HOST = "localhost"
+
+dp = Dispatcher()
 
 
-async def on_startup(bot: Bot) -> None:
-    await bot.set_webhook(f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}")
-
-
-def main() -> None:
-    dp = Dispatcher()
-    dp.include_router(begin_router)
-
-    dp.startup.register(on_startup)
-
+async def main() -> None:
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
-    app = web.Application()
-
-    webhook_requests_handler = SimpleRequestHandler(
-        dispatcher=dp,
-        bot=bot,
-
-    )
-    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
-
-    setup_application(app, dp, bot=bot)
-
-    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
+    dp.include_router(begin_router)
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    main()
+    asyncio.run(main())
+
